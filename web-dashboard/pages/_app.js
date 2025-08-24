da337be61c3bff49478e82555bdc5ee243843545
JorgeMabuie <jorgemaabuie@gmail.com>
2025-08-24 08:22:20 +0200

    Update _app.js

--- web-dashboard/pages/_app.js
+++ web-dashboard/pages/_app.js
@@ -1,18 +1,26 @@
+// pages/_app.js
 import '../styles/globals.css';
 import 'leaflet/dist/leaflet.css';
 import { useEffect } from 'react';
 
-export default function MyApp({ Component, pageProps }) {
-  // Leaflet icon fix
+function MyApp({ Component, pageProps }) {
   useEffect(() => {
+    // global leaflet icon fix in client only
     import('leaflet').then(L => {
-      delete L.Icon.Default.prototype._getIconUrl;
-      L.Icon.Default.mergeOptions({
-        iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-        iconUrl: require('leaflet/dist/images/marker-icon.png'),
-        shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-      });
-    });
+      try {
+        delete L.Icon.Default.prototype._getIconUrl;
+        L.Icon.Default.mergeOptions({
+          iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+          iconUrl: require('leaflet/dist/images/marker-icon.png'),
+          shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+        });
+      } catch (e) {
+        // ignore if run inside server during build (should not), or if images not found
+      }
+    }).catch(() => {});
   }, []);
+
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
